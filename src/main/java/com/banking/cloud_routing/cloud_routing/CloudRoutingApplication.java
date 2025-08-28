9964b80b208c168a2a5206209b1389fe7a7b0bec
JavGog <javierggjavier@gmail.com>
2025-08-28 17:42:48 +0200

    commit 3

--- src/main/java/com/banking/cloud_routing/cloud_routing/CloudRoutingApplication.java
+++ src/main/java/com/banking/cloud_routing/cloud_routing/CloudRoutingApplication.java
@@ -12,23 +12,23 @@ public class CloudRoutingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CloudRoutingApplication.class, args);
 	}
-//	@Bean
-//	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-//		return builder.routes()
-//				.route(r -> r.path("/account/**")
-//						.filters(f -> f
-//							.prefixPath("/api")
-//							.addResponseHeader("X-Powered-By","DanSON Gateway Service")
-//						)
-//						.uri("http://localhost:8081")
-//				)
-//				.route(r -> r.path("/customers/**")
-//						.filters(f -> f
-//								.prefixPath("/api")
-//								.addResponseHeader("X-Powered-By","DanSON Gateway Service")
-//						)
-//						.uri("http://localhost:8082")
-//				)
-//				.build();
-//	}
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(r -> r.path("/account/**")
+						.filters(f -> f
+							.prefixPath("/api")
+							.addResponseHeader("X-Powered-By","DanSON Gateway Service")
+						)
+						.uri("http://localhost:8081")
+				)
+				.route(r -> r.path("/customers/**")
+						.filters(f -> f
+								.prefixPath("/api")
+								.addResponseHeader("X-Powered-By","DanSON Gateway Service")
+						)
+						.uri("http://localhost:8082")
+				)
+				.build();
+	}
 }
